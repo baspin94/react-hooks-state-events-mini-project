959c088d258f188bcc57b43cc13460ef77e41196
Bianca Aspin <bianca@mj94.net>
2023-02-05 23:48:36 -0800

    Create button component and render category buttons.

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES}/>
       <NewTaskForm />
       <TaskList tasks={tasks} onTaskDelete={handleDelete}/>
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Button from "./Button";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const categoryButtons = categories.map(category => {
+    return <Button key={category} name={category} />
+  })
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
