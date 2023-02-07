6a381154b341c784c63b6f0ec49606ffafadf869
Bianca Aspin <bianca@mj94.net>
2023-02-06 17:05:57 -0800

    Add event listeners to form input and dropdown.

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewTaskForm({categories}) {
+  const [newTask, setNewTask] = useState("")
+  const [newTaskCategory, setNewTaskCategory] = useState("Code");
 
   const categoryOptions = categories
     .filter(category => category !== "All")
     .map(category => {
-      return <option>{category}</option>
+      return <option key={category}>{category}</option>
     });
+
+  function handleChange(event) {
+    setNewTaskCategory(event.target.value);
+
+  }
+
+  function handleInput(event) {
+    setNewTask(event.target.value);
+  }
     
   return (
     <form className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={newTask} onInput={handleInput}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" value={newTaskCategory} onChange={handleChange}>
           {categoryOptions}
         </select>
       </label>
